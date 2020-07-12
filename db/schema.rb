db4b19f0eb8d916b37444f58111ce6cc030f898c
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-13 03:41:07 +0900

    Create users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_12_123322) do
+ActiveRecord::Schema.define(version: 2020_07_12_130711) do
 
   create_table "departments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "employee_number", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.bigint "department_id"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["department_id"], name: "index_users_on_department_id"
+    t.index ["employee_number"], name: "index_users_on_employee_number", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "users", "departments"
 end
