b94886b48c9246df208fcc4b5d9b294b35917ad8
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-13 03:39:56 +0900

    Create user model
    & make association with department.rb

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :validatable
+  
+  validates :name, presence: true
+  validates :employee_number, presence: true, uniqueness: true
+
+  belongs_to :department
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+
+  def will_save_change_to_email?
+    false
+  end
+end
