41964566f9f660f44018335037de1fe9b32d3c0c
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-15 06:02:49 +0900

    Create evaluation data table

---
+++ app/models/evaluation_datum.rb
@@ -0,0 +1,3 @@
+class EvaluationDatum < ApplicationRecord
+  belongs_to :user
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   belongs_to :department
   has_many   :production_data
   has_many   :inspection_data
+  has_many   :evaluation_data
 
   def email_required?
     false
