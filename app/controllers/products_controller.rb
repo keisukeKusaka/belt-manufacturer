bf599864d7ae63f9a9180b3e30c61aa67cfe9368
boborabi <65844843+boborabi@users.noreply.github.com>
2020-07-16 23:06:15 +0900

    Revert "基本機能の実装"

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,15 +1,4 @@
 class ProductsController < ApplicationController
   def index
   end
-
-  def new
-  end
-
-  def create
-    @product = Product.new(product_params)
-  end
-
-  private
-  def product_params
-  end
 end
